f40b2a03a34898b87697b01aaf35307097bfa1c4
Victor Aromose <48617966+VikkyDev05@users.noreply.github.com>
2022-05-25 04:03:07 +0000

    . . .

--- js/script.js
+++ js/script.js
@@ -24,81 +24,29 @@ function letSee() {
 
     
   };
-}
 
   //OPTIONAL
   
-  // let newRequest = new XMLHttpRequest();
-  // let newUrl = "https://api.adviceslip.com/advice/search/"+searchInput;
-  // newRequest.open("GET", newUrl, true);
-  // newRequest.onload = function() {
+  let newRequest = new XMLHttpRequest();
+  let newUrl = "https://api.adviceslip.com/advice/search/"+searchInput;
+  newRequest.open("GET", newUrl, true);
+  newRequest.onload = function() {
 
-  //   let newRes = JSON.parse(this.response);
-  //   if (newRequest.status >= 200 && newRequest.status < 400) {
-  //     let resAdv = newRes.slips[0].advice;
-  //     console.log(resAdv);
-  //     $("#advice").text(resAdv);
-  //   }
+    let newRes = JSON.parse(this.response);
+    if (newRequest.status >= 200 && newRequest.status < 400) {
+      let resAdv = newRes.slips[0].advice;
+      console.log(resAdv);
+      $("#advice").text(resAdv);
+    }
 
     
-  // };
+  };
   
   
 
-//   request.send();
-//   newRequest.send();
-// }
-
-
-
-// let newRequest = new XMLHttpRequest();
-
-// let newUrl = "https://api.adviceslip.com/advice";
-
-
-// newRequest.open("GET", newUrl, true);
-
-// newRequest.onload = function() {
-// let newData = JSON.parse(this.response);
-
-  
-// };
-// newRequest.onload = function() {
-
-//   let newData = JSON.parse(this.response);
-//  if (newRequest.status >= 200 && newRequest.status < 400) {
-//           $("#advice").text(newData["slip"].advice);
-//                 }
-
-// };
-
-// newRequest.send();
-
-// function reset() {
-//   let newRequest = new XMLHttpRequest();
-
-//   let newUrl = "https://api.adviceslip.com/advice";
-  
-  
-//   newRequest.open("GET", newUrl, true);
-  
-//   newRequest.onload = function() {
-//   let newData = JSON.parse(this.response);
-  
-    
-//   };
-  
-//   newRequest.onload = function() {
-  
-//     let newData = JSON.parse(this.response);
-//    if (newRequest.status >= 200 && newRequest.status < 400) {
-//             $("#advice").text(newData["slip"].advice);
-//                   }
-  
-//   };
-  
-//   newRequest.send();
-// }
+  request.send();
+  newRequest.send();
+}
 
 //Activation Key 
 //rVeCO4fnPWt76cdVHVt5fB43PzHqwKOa
